186b33cda12e2fa4acdc89c6038d4e47fe9434b0
kitdim <kitaykin9808@gmail.com>
2023-10-23 12:39:15 +0300

    init commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:5.6.0")
     implementation("gg.jte:jte:3.0.1")
+    implementation("net.datafaker:datafaker:2.0.1")
+    implementation("org.projectlombok:lombok:1.18.28")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,15 +1,55 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import net.datafaker.Faker;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.model.Course;
+
+import java.util.Collections;
+import java.util.List;
 
 public class HelloWorld {
+    private static final List<Course> COURSES = new Faker().collection()
+            .suppliers(
+                    () -> new Faker().number().numberBetween(1, 100),
+                    () -> new Faker().name().firstName(),
+                    () -> new Faker().animal().scientificName())
+            .minLen(5)
+            .maxLen(20)
+            .build().get();
+
     public static void main(String[] args) {
         // Создаем приложение
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+        app.get("/", ctx -> ctx.render("index.jte"));
+        app.get("/courses", ctx -> {
+            var id = ctx.formParamAsClass("id", Integer.class);
+            var courses = COURSES;
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", Collections.singletonMap("page", page));
+        });
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.formParamAsClass("id", Integer.class);
+            var course = COURSES.get(id.get());
+            var page = new CoursePage(course);
+            ctx.render("courses/show.jte", Collections.singletonMap("page", page));
+        });
+
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            if (name != null) {
+                ctx.result("Hello ".concat(name));
+            } else {
+                ctx.result("Hello world");
+            }
+        });
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+        });
+        app.start(7070);
     }
 }
